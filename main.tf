ea510456316443e32742d6d0d99673dd0ac4bb32
bleons36 <89231748+bleons36@users.noreply.github.com>
2024-02-15 15:49:15 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 //--------------------------------------------------------------------
 // Modules
+
 module "network" {
   source  = "app.terraform.io/sulee/network/aws"
   version = "1.0.0"
-  name = "test-vpc"
-  azs = ["ap-northeast-2a","ap-northeast-2b","ap-northeast-2c"]
-  cidr = "10.10.0.0/16"
+  name                    = "test-vpc"
+  cidr                    = "10.10.0.0/16"
+  azs                     = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
 }
+
+#resource "aws_instance" "example" {
+#  ami           = "ami-00ba43a774eb5870b"
+#  instance_type = "t2.micro"
+#}
