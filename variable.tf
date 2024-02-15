8bc34b9a24d609e2db588b7f3100485f1c3777de
bleons36 <89231748+bleons36@users.noreply.github.com>
2024-02-15 16:30:15 +0900

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+}
+
+variable "TFC_ORGANIZATION_NAME" {
+  type    = string
+  default = "sulee"
+}
